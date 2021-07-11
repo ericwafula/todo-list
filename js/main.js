6607ea2e6c76227045e3da59de9443f0cd81505c
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-11 19:15:30 +0300

    added initial guide text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // UI Logic
 $(document).ready(function(){
     $(".btn-add-to-do").click(function(){
-        $("#cards-container").prepend('<div class=cards></div>');
+        $("#cards-container").prepend('<div class="cards"><p id="card-title">Title</p>' + 
+        '<p id="card-description">Brief description about the...</p>' +
+        '</div>');
+        $("#click-the").remove();
     });
 });
