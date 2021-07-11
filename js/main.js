2e6a2f11bbbb6a4087c4b096d87ce089f1898d33
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-12 01:58:53 +0300

    added remove card functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 // UI Logic
 $(document).ready(function(){
+    // add to do button
     $(".btn-add-to-do").click(function(){
-        $("#cards-container").prepend('<div class="cards"><p id="card-title">Title</p>' + 
-        '<p id="card-description">Brief description about the...</p>' +
-        '</div>');
+        $("#cards-container").prepend('<div class="cards">' + 
+            '<div class ="card-head">' + 
+                '<p id="card-title">Title</p>' +
+                '<span class="material-icons-outlined">clear</span>' +
+            '</div>' +
+            '<p id="card-description">Brief description about the...</p>' +
+            '</div>');
         $("#click-the").remove();
         $("#lp-heading").hide();
         $("#illustration-image").hide();
+
+        // card
+        // mouse over
+        $("div#cards-container").children("div").first().mouseover(function(){
+            $(".material-icons-outlined").show();
+        });
+
+        // mouse out
+        $("div#cards-container").children("div").first().mouseout(function(){
+            $(".material-icons-outlined").hide();
+        });
+
+        $(".cards").find(".material-icons-outlined").first().click(function(){
+            $(this).parents(".cards").remove();
+        });
     });
+
+    
 });
